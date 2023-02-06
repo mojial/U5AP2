111fcb2b713461a063e6f9bc773d0128b395d30a
mojial <mojial@floridauniversitaria.es>
2023-02-06 09:14:41 +0100

    Ejercicio 2

--- ficheros_scripts/segundo.sh
+++ ficheros_scripts/segundo.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
+total=cat ./ficheros_scripts/numeros.txt | wc -l
 
+echo "PARES"
+for i in seq 1 $total
+do
+  linea=cat ./ficheros_scripts/numeros.txt | head -n$i | tail -n1
+  resto=$((linea%2))
+  if [ $resto -eq 0 ]
+  then
+    echo "$linea es par"
+  fi
+done
+
+echo "IMPARES"
+for i in seq 1 $total
+do
+  linea=cat ./ficheros_scripts/numeros.txt | head -n$i | tail -n1
+    resto=$((linea%2))
+    if [ $resto -ne 0 ]
+  then
+    echo "$linea es impar"
+  fi
+done
