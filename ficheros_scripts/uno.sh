10924f88d35376381aca845f772d7bc121fb6900
mojial <mojial@floridauniversitaria.es>
2023-02-06 09:32:32 +0100

    Ejercicio 1 corregido

--- ficheros_scripts/uno.sh
+++ ficheros_scripts/uno.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-num_subdirs=$1
+$total='cat nombres.txt | wc -l'
 
-if [ -z "$num_subdirs" ]; then
-  echo "Error: debe proporcionar el número de subdirectorios personal como argumento."
-  exit 1
-fi
-
-
-if [ ! -f "nombres.txt" ]; then
-  echo "Error: no se encontró el fichero nombres.txt."
-  exit 1
-fi
-
-
-while read nombre; do
-  mkdir -p "$nombre"
-  for i in $(seq 1 "$num_subdirs"); do
-    mkdir -p "$nombre/Personal $i"
+for i in 'seq 1 $total'
+do
+  nombre='cat nombres.txt | head -n$i | tail -n1'
+  mkdir $nombre
+  for i in 'seq 1 $1'
+  do
+    mkdir ./$nombre/persona1$j
   done
-done < nombres.txt
-
-echo "La estructura de directorios se ha creado exitosamente."
+done
